773e264a67558f5ecf854c159a937661f0aefb27
WFNott <walthersat@gmail.com>
2023-04-04 11:16:17 -0500

    Finish Create Table & Entitie "Country"

---
+++ Shopping/Data/DataContex.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    // Esta clase esta hecha para la conexion a la base de datos
+    public class DataContex : DbContext
+    {
+
+        // Se crea un constructor
+
+        public DataContex(DbContextOptions<DataContex> options): base(options)
+        {
+            
+        }
+
+        // Creo un DbSet para mapear la entidad, esto quiere decir que convierte a un objeto a una entidad
+        // legible por el Entity framework y se le asigna el nombre 
+
+        public DbSet<Country> countries { get; set; }
+
+
+        // El "OnModelCreating" es un metodo para modificar o crear en la tabla
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+
+}
---
+++ Shopping/Data/Entities/Country.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        // En entiti framework se exige una PK, en nuestro caso sera el ID del pais
+        public int Id { get; set; }
+
+        // Para evitar ingresos errorneos del usuario se usan metodos para limitar sus respuestas
+
+        /* "Display" sirve para mostrar una propiedad con un nombre personalizado para el usuario
+         sin comprometer el apartado tecnico*/
+        [Display(Name = "Pais")]
+
+        /* El "MaxLength" se crea para restringir la capacidad de los caracteres para la base de datos
+         EN caso de que el usuario agrege mas de la capacidad permitida, se dispara el "Error Message"
+         el cual mostrara un mensaje personalizado, el "{0}" y "{1}" agregara en orden la información y parametros definidas*/
+        [MaxLength(50, ErrorMessage = "El {0} no puedes superar los {1} caracteres" )]
+
+        // El "Required" se usa para obligar al usuario llenar el campo
+
+        [Required(ErrorMessage = "El {0} es obligatorio")]
+
+        public string Name { get; set; }
+
+    }
+}
