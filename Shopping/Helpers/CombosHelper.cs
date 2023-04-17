1b4cb5ca79766af8db696016e3ab0d37cac77d79
WFNott <walthersat@gmail.com>
2023-04-17 10:49:14 -0500

    Create CombosHelper

---
+++ Shopping/Helpers/CombosHelper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Shopping.Data;
+using Shopping.Data.Entities;
+
+namespace Shopping.Helpers
+{
+	public class CombosHelper : ICombosHelper
+	{
+		private readonly DataContex _context;
+
+		public CombosHelper(DataContex context)
+        {
+			_context = context;
+		}
+
+		public async Task<IEnumerable<SelectListItem>> GetComboCategoriesAsync()
+		{
+			List<SelectListItem> list = await _context.categories.Select(c => new SelectListItem
+			{
+				Text = c.Name,
+				Value = c.Id.ToString()
+			}).OrderBy(c => c.Text)
+				.ToListAsync();
+			list.Insert(0, new SelectListItem { Text = "Seleccione una Categoria...", Value ="0"});
+			return list;
+		}
+
+		public async Task<IEnumerable<SelectListItem>> GetComboCitiesAsync(int stateId)
+		{
+			List<SelectListItem> list = await _context.cities.Where(s => s.State.Id == stateId).Select(c => new SelectListItem
+			{
+				Text = c.Name,
+				Value = c.Id.ToString()
+			}).OrderBy(c => c.Text)
+				.ToListAsync();
+			list.Insert(0, new SelectListItem { Text = "Seleccione una ciudad...", Value = "0" });
+			return list;
+		}
+
+		public async Task<IEnumerable<SelectListItem>> GetComboCountriesAsync()
+		{
+			List<SelectListItem> list = await _context.countries.Select(c => new SelectListItem
+			{
+				Text = c.Name,
+				Value = c.Id.ToString()
+			}).OrderBy(c => c.Text)
+					.ToListAsync();
+			list.Insert(0, new SelectListItem { Text = "Seleccione un Pais...", Value = "0" });
+			return list;
+		}
+
+		public async Task<IEnumerable<SelectListItem>> GetComboStatesAsync(int countryId)
+		{
+			List<SelectListItem> list = await _context.states.Where(s => s.Country.Id == countryId).Select(c => new SelectListItem
+			{
+				Text = c.Name,
+				Value = c.Id.ToString()
+			}).OrderBy(c => c.Text)
+				.ToListAsync();
+			list.Insert(0, new SelectListItem { Text = "Seleccione un Departamento/Estado...", Value = "0" });
+			return list;
+		}
+	}
+}
